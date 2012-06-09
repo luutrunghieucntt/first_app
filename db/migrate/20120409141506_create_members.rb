68224e903cdb97d0feed4f3b57975f2aa8e666bd
USBHY <usbhy@USBHY>
2012-06-09 10:53:41 +0700

    update mirage

--- db/migrate/20120409141506_create_members.rb
+++ db/migrate/20120409141506_create_members.rb
@@ -1,7 +1,7 @@
 class CreateMembers < ActiveRecord::Migration
   def change
     create_table :members do |t|
-      t.column :status, "ENUM('init', 'success', 'fail')"
+      t.column :status, :string
 
       t.timestamps
     end
